b09eb115c24164b1d4fed67ad8c699c37cdb0e5e
dinh-hung-116 <nhudinhhung2005@gmail.com>
2026-04-10 20:55:43 +0700

    update: - Repo mới

---
+++ chess/gui/Class/UserInfoPanel.java
@@ -0,0 +1,25 @@
+package chess.gui.Class;
+
+
+import chess.gui.Class.guiUtils;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+// Lớp này chứa tên của User trong trận đấu
+public class UserInfoPanel extends JPanel {
+
+    // label để test tên
+    public UserInfoPanel(String label) {
+        this.setPreferredSize(guiUtils.USER_FRAME_DIMENSION);
+        this.setLayout(new BorderLayout());
+       
+        this.setBackground(Color.GRAY);
+        this.add(new JLabel(label));
+        
+        this.setVisible(true);
+    }
+    
+}
